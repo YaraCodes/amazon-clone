93790be685354d5376dcecf8f9d524b634fc7bb5
Yashraj <yashrajrvengurlekar@gmail.com>
2022-03-12 13:03:54 +0530

    step 2 completed- Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./Home";
+import Home from "./Component/Home";
+import Header from "./Component/Header";
 function App() {
 return (
   <div className="app">
     <Router>
       <Switch>
         <Route path="/">
-          <Home />
+       
+            <Header />
+            <Home />
         </Route>
       </Switch>
     </Router>
